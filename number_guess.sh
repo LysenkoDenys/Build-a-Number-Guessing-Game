f238b65379666a482f030b371a2ec227f480de6f
Lysenko <lysenkoden@gmail.com>
2025-11-15 03:07:26 -0500

    fix: fixed guess block

--- number_guess.sh
+++ number_guess.sh
@@ -39,14 +39,14 @@ read USERNAME
 				else
 # increment guess counter if input is valid:
 					((GUESSES++))
-					if (( USER_INPUT > SECRET_NUMBER )); then
-							echo "It's higher than that, guess again:"
-					elif (( USER_INPUT < SECRET_NUMBER )); then
-							echo "It's lower than that, guess again:"
+					if [[ $USER_INPUT -gt $SECRET_NUMBER ]]; then
+						echo "It's higher than that, guess again:"
+					elif [[ $USER_INPUT -lt $SECRET_NUMBER ]]; then
+						echo "It's lower than that, guess again:"
 					else
-							echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-							$PSQL "INSERT INTO games (user_id, guesses) VALUES($USER_ID, $GUESSES);" > /dev/null
-							exit
+						echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+						$PSQL "INSERT INTO games (user_id, guesses) VALUES($USER_ID, $GUESSES);" > /dev/null
+						exit
 					fi
 				fi
 		done
